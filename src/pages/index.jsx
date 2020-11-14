a0e7111a60d34e8f7046277eba7ff67261375654
小俣圭佑 <omatakeisuke@MacBook.local>
2020-11-14 17:38:42 +0900

    fix

---
+++ src/pages/index.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+export default function Home({ blog }) {
+  return (
+    <div>
+      {blog.map(blog => (
+        <ul key={blog.id}>
+          <li >
+            <Link href={`posts/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch('https://isrbrog.microcms.io/api/v1/posts', key)
+    .then(res => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
