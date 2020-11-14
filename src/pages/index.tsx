3d3352338e857a44c2dd649d7b67a0d7bf366815
小俣圭佑 <omatakeisuke@MacBook.local>
2020-11-14 15:27:24 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,27 @@
-import Link from 'next/link';
-
-export default function Home({ post }) {
+function Blog({ posts }) {
   return (
-    <div>
-      {post.map(blog => (
-        <ul key={blog.id}>
-          <li >
-            <Link href={`posts/${blog.id}`}>
-              <a>{blog.title}</a>
-            </Link>
-          </li>
-        </ul>
+    <ul>
+      {posts.map((post) => (
+        <li>{post.title}</li>
       ))}
-    </div>
-  );
+    </ul>
+  )
 }
 
-export const getStaticProps = async () => {
-  try {
-    const key = {
-      headers: {'X-API-KEY': process.env.API_KEY},
-    };
-    const result = await fetch('https://isrbrog.microcms.io/api/v1/posts', key)
-    const postContents = await result.json()
-    const post = postContents.contents
-    return {
-      props: { post },
-      revalidate: 60,
-    }
-  } catch (e) {
-    console.log({ e })
+export async function getStaticProps() {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const res = await fetch('https://isrbrog.microcms.io/api/v1/posts', key)
+  const postsContents = await res.json()
+  const posts = postsContents.contents
+
+  return {
+    props: {
+      posts: posts || null,
+    },
+    revalidate: 1,
   }
 }
+
+export default Blog
