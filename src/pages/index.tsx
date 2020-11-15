792cb783bc799475b1c5c0ae2f4fd8325c791e2b
小俣圭佑 <omatakeisuke@MacBook.local>
2020-11-15 17:01:18 +0900

    [edit] index.tsxの表記を修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import Link from 'next/link';
 
+export const getStaticProps = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch('https://isrbrog.microcms.io/api/v1/posts', key)
+    .then(res => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
 export default function Home({ blog }) {
   return (
     <div>
@@ -15,17 +29,3 @@ export default function Home({ blog }) {
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  const key = {
-    headers: {'X-API-KEY': process.env.API_KEY},
-  };
-  const data = await fetch('https://isrbrog.microcms.io/api/v1/posts', key)
-    .then(res => res.json())
-    .catch(() => null);
-  return {
-    props: {
-      blog: data.contents,
-    },
-  };
-};
