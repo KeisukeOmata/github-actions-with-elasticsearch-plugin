9b8ddf69e660e7de37c17ba600598ad170e10106
小俣圭佑 <omatakeisuke@MacBook.local>
2020-11-15 14:24:49 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home({ blog }) {
 
 export const getStaticProps = async () => {
   const key = {
-    headers: {'X-API-KEY': process.env.API_KEY},
+    headers: {'X-API-KEY': `${process.env.API_KEY}`},
   };
   const data = await fetch('https://isrbrog.microcms.io/api/v1/posts', key)
     .then(res => res.json())
