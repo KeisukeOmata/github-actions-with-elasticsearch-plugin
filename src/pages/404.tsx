6dc7ae52654b54f488cb69bf61859749d7aa808a
小俣圭佑 <omatakeisuke@MacBook.local>
2020-11-23 14:31:08 +0900

    [add] 404ページを追加

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,18 @@
 import { NextPage } from 'next'
+import { ContentWrapper } from "@src/layouts/ContentWrapper";
+import { BackTop } from "@src/components/BackTop";
 
 const Custom404: NextPage = () => {
   return (
-    <h1>
-      404 - Page Not Found
-    </h1>
+    <div className="error">
+      <ContentWrapper>
+        <div className="error__status">404</div>
+        <h1 className="error__message">Page not found...</h1>
+        <nav className="error__actions">
+          <BackTop />
+        </nav>
+      </ContentWrapper>
+    </div>
   )
 }
 
