3d4b5d9bca28bb5610f529f00d3c172bb4d7321e
小俣圭佑 <omatakeisuke@MacBook.local>
2020-11-28 15:51:17 +0900

    Add header

--- next_blog/src/pages/_app.tsx
+++ next_blog/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import '@src/styles/globals.scss'
+import { SiteHeader } from "@src/layouts/SiteHeader";
 import { SiteFooter } from "@src/layouts/SiteFooter";
 import { FixedFooter } from "@src/layouts/FixedFooter";
 import { Space } from "@src/components/Space";
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <FixedFooter>
+        <SiteHeader />
         <Component {...pageProps} />
         <Space />
         <SiteFooter />
