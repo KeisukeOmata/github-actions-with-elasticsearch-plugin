61c8513fab10b90c4b4b45ef2779d6d3102df1d4
小俣圭佑 <omatakeisuke@MacBook.local>
2020-12-09 08:19:49 +0900

    fix

--- next_blog/jest.config.js
+++ next_blog/jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   roots: ['<rootDir>/src'],
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
-    // pages と components ディレクトリのエイリアスを設定（必要であれば他のディレクトリも追加）
     '^(pages|components|layouts)/(.+)': '<rootDir>/src/$1/$2',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
